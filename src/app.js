7ef0f23262c80718becf812f0dae19936898500e
lizhenhao <ypeuskskli@computime.com>
2019-08-24 14:58:59 +0800

    chore: init

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import express from 'express'
+
+const app = express()
+
+app.all('*', (req, res, next) => {
+  const { origin, Origin, referer, Referer } = req.headers
+  const allowOrigin = origin || Origin || referer || Referer || '*'
+  console.log('allowOrigin: ' + allowOrigin)
+  res.header('Access-Control-Allow-Origin', allowOrigin)
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With')
+  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
+  res.header('Access-Control-Allow-Credentials', true) // 可以带cookies
+  res.header('X-Powered-By', 'Express')
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200)
+  } else {
+    next()
+  }
+})
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+var server = app.listen(8081, function () {
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('listen start')
+  console.log('host', host)
+  console.log('port', port)
+})
