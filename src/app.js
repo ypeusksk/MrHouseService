f7fe48354b3dc9b91b8e37e0534c3866058ada30
lizhenhao <ypeuskskli@computime.com>
2019-10-07 18:01:49 +0800

    fix: fix the bug

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 import express from 'express'
 import router from './router'
+import BodyParser from 'body-parser'
 
 const app = express()
+const limit = '20mb'
+app.use(BodyParser.urlencoded({ limit, extended: true }))
+app.use(BodyParser.json({ limit }))
 router(app)
 
 var server = app.listen(8081, function () {
