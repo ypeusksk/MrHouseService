5fa4217218c30a8cbbc889254fa8ec5507b2e9a8
lizhenhao <ypeuskskli@computime.com>
2019-11-18 11:56:15 +0800

    refactor

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import user from '../controller/user'
 import wx from '../controller/wx'
 import BodyParser from 'body-parser'
-import multipart from 'connect-multiparty'
 
 function router (app) {
   app.all('*', (req, res, next) => {
@@ -20,21 +19,9 @@ function router (app) {
   })
 
   const urlencodedParser = BodyParser.urlencoded({ extended: false })
-  const multipartMiddleware = multipart()
 
   app.post('/login', urlencodedParser, user.login)
   app.post('/call/access_token/:access_token/env/:env/name/:name', urlencodedParser, wx.call)
-  app.post('/testFormDataUpload', multipartMiddleware, function (req, res) {
-    // 键值对内容
-    console.log('req.body', req.body)
-
-    // 文件内容
-    console.log('req.files', req.files)
-
-    // 此处存储上传的文件内容
-
-    res.send('此处可以返回存储后的文件路径')
-  })
 }
 
 export default router
