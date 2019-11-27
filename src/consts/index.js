1553670c462ade5f630e73cffb03d10c3f0d7f90
lizhenhao <ypeuskskli@computime.com>
2019-11-27 14:30:19 +0800

    feat: update env

--- src/consts/index.js
+++ src/consts/index.js
@@ -2,7 +2,7 @@ export default {
   DB_NAME: 'MrHouse',
   DB_URL: 'mongodb://localhost:27017/MrHouse',
   WX_LOGIN: {
-    APPID: 'wx16694564f83833d3',
-    SECRET: 'b2f23ccbbc7254bf474a650a105e5ef1'
+    APPID: 'wx8973f4f35fa9898b',
+    SECRET: 'a2545df80d00a4c12a4eeca877653133'
   }
 }
