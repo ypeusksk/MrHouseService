14ac86e58c4861251086117081bfc12915489d93
李振豪 <554749460@qq.com>
2021-06-16 10:28:39 +0800

    feat: add log

--- src/controller/user.js
+++ src/controller/user.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 import consts from '../consts'
 
 function login (req, res) {
+  consts.LOG && console.log('login listent')
   const { username, password } = req.body
   user.checkUsernamePassword(username, password).then(() => {
     consts.LOG && console.log('用户密码正确，开始调用微信小程序的登录api')
