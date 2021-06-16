af7a541d35de4021fd338d22f7d1e7e9ac91f416
李振豪 <554749460@qq.com>
2021-06-16 10:25:00 +0800

    feat: add log

--- src/controller/user.js
+++ src/controller/user.js
@@ -5,8 +5,10 @@ import consts from '../consts'
 function login (req, res) {
   const { username, password } = req.body
   user.checkUsernamePassword(username, password).then(() => {
+    consts.LOG && console.log('用户密码正确，开始调用微信小程序的登录api')
     const url = `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${consts.WX_LOGIN.APPID}&secret=${consts.WX_LOGIN.SECRET}`
     axios.get(url).then(response => {
+      consts.LOG && console.log('login response', response)
       res.send(response.data)
     })
   }, () => {
